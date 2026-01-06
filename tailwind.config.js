cb8afa7fd7cc02859c7c260ef70ca18f61f1e479
Oriol Cardona Alsina <ocardonaa@uoc.edu>
2026-01-06 20:56:02 +0100

    setup Tailwind + header done

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./*.html",
+    "./src/**/*.scss"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} 
