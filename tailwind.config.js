9dfbfe9fbaf2b03254d8e13673ce7fdd2b400234
Oriol Cardona Alsina <ocardonaa@uoc.edu>
2026-01-09 18:05:07 +0100

    extracted to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,35 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'uoc-backg': "color-mix(in srgb, #73edff, white 70%)",
+        'uoc-corporate': '#000078',
+        'uoc-masterbrand': '#73edff',
+        'white': '#ffffff',
+        'lightgrey': '#D3D3D3'
+      },
+      screens: {
+        sm: '360px',
+        md: '480px',
+        lg: '768px',
+      },
+      spacing: {
+        '15%': '15%',
+      },
+      fontFamily: {
+        sans: [
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+        ],
+      },
     },
   },
   plugins: [],
